ea224131b02d765e764327ad133d20c6a95595af
Paul Onteri <me@paulonteri.com>
2022-02-28 19:21:41 +0300

    add basic structure

---
+++ freelance/partials/head.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freelance Marketplace</title>
+    <link rel="stylesheet" href="static/css/index.css">
+    <link rel="stylesheet" href="static/css/default.css">
+</head>
---
+++ freelance/partials/navbar.php
@@ -0,0 +1,8 @@
+   <!-------------- ------------------ navbar -------------------------------------------------------->
+   <div class="topnav">
+       <a href="/">Freelance marketplace</a>
+       <a href="/freelancers">Freelancers</a>
+       <a href="/jobs">Jobs</a>
+       <a href="/login">Login</a>
+   </div>
+   <!-------------- ------------------ end navbar -------------------------------------------------------->
