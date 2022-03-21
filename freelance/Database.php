0cf3b86ad20d8db767a602e2e2702bc6bdb3e15e
Paul Onteri <me@paulonteri.com>
2022-03-21 17:09:04 +0300

    db errormode

--- freelance/Database.php
+++ freelance/Database.php
@@ -7,16 +7,17 @@ use PDO;
 class Database
 {
 
-    private $host = '192.168.64.2';
-    private $user = 'freelance';
-    private $password = 'freelance';
-    private $dbName = 'freelance';
+  private $host = '192.168.64.2';
+  private $user = 'freelance';
+  private $password = 'freelance';
+  private $dbName = 'freelance';
 
-    protected function connectToDb() {
-      $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
-      $pdo = new PDO($dsn, $this->user, $this->password);
-      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-      return $pdo;
-    }
-
+  protected function connectToDb()
+  {
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+    $pdo = new PDO($dsn, $this->user, $this->password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $pdo;
   }
+}
