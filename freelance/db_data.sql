c49fa02d15c7b6f0eb5a4561b27e181ce9689007
Paul Onteri <me@paulonteri.com>
2022-03-24 11:37:36 +0300

    data export

--- freelance/db_data.sql
+++ freelance/db_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 23, 2022 at 06:22 AM
+-- Generation Time: Mar 24, 2022 at 09:36 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -28,7 +28,7 @@ USE `freelance`;
 --
 
 INSERT INTO `client` (`id`, `user_id`, `image`, `title`, `description`, `time_created`, `is_active`) VALUES
-(2, 11, '/uploads/ClientImageTueMar2220227:05amazon.png', 'Recusandae Impedit', 'Dolor animi aut qui', '2022-03-22 07:05:12', 1),
+(2, 11, '/uploads/ClientImageTueMar2220227:05amazon.png', 'Nakuru Mattress', 'A homegrown supermarket established in 2006 with our first branch in Nakuru town. We\'ve later grown and so far have a total of 46 branches in 12 counties across Kenya.\n\nWe pride ourselves with delivering an exceptional customer experience every time while providing our shopper with variety of goods at an affordable price!\n\nVisit any of our branches today and enjoy delectable pastries from our Bakery, sumptuous meals at our Deli, fresh cuts at the Butchery and so much more!\n\nUnique Value Proposition:\n\nFresh & Easy. Price Guarantee!', '2022-03-22 07:05:12', 1),
 (3, 13, '/uploads/ClientImageTueMar2220229:371628513616264.jpeg', 'Ut mollitia excepteu', 'Est facilis maxime a', '2022-03-22 09:06:43', 1);
 
 --
@@ -53,13 +53,44 @@ INSERT INTO `freelancer_skill` (`id`, `freelancer_id`, `skill_id`) VALUES
 (6, 11, 3);
 
 --
+-- Dumping data for table `job`
+--
+
+INSERT INTO `job` (`id`, `client_id`, `title`, `description`, `image`, `pay_rate_per_hour`, `expected_duration_in_hours`, `receive_job_proposals_deadline`, `time_created`, `is_active`) VALUES
+(5, 2, 'Social media posts', 'Person responsible for overseeing a company’s interactions with the public by implementing social media platforms’ content strategies. Their duties include analyzing engagement data, identifying trends in customer interactions and planning digital campaigns to build community online.', '/uploads/ClientJobImageWedMar2320227:56images.png', 20, 5, '2022-03-29 15:07:00', '2022-03-23 07:56:09', 1),
+(6, 2, 'Flutter App developer', 'Looking for a flutter app developer to assist in updating and correcting a few bugs on an existing App, Should be able to code and understand best practices in Flutter and Dart and can deploy IOS and Android on the Play Store and Appstore. An Understanding of Firebase, Git, and API interaction with the backend and app testing too is key. &bull; Developed 2+ iOS/Android apps with Flutter, either deployed on the AppStore/Google Play or available on Github', '/uploads/ClientJobImageThuMar2420226:14images.png', 2000, 20, '2022-04-30 09:11:00', '2022-03-24 06:14:02', 1);
+
+--
+-- Dumping data for table `job_proposal`
+--
+
+INSERT INTO `job_proposal` (`id`, `status`, `title`, `description`, `job_id`, `freelancer_id`, `client_comment`, `time_work_starts`, `time_work_ends`, `time_created`, `is_active`) VALUES
+(1, 'sent', 'Sunt nulla non accu', 'Nemo doloribus volup', 6, 10, NULL, NULL, NULL, '2022-03-24 08:36:04', 1);
+
+--
+-- Dumping data for table `job_skill`
+--
+
+INSERT INTO `job_skill` (`id`, `job_id`, `skill_id`) VALUES
+(7, 5, 8),
+(8, 6, 1);
+
+--
 -- Dumping data for table `skill`
 --
 
 INSERT INTO `skill` (`id`, `name`) VALUES
-(4, 'Graphic Design'),
+(11, 'Accounting'),
+(9, 'DataEntry'),
+(4, 'GraphicDesign'),
+(5, 'ITSupport'),
+(7, 'LegalAssistant'),
 (1, 'Programming'),
+(8, 'SocialMediaManager'),
+(12, 'Transcribing'),
+(6, 'Translator'),
 (2, 'UI/UX'),
+(10, 'UsabilityTesting'),
 (3, 'Writing');
 
 --
@@ -67,8 +98,8 @@ INSERT INTO `skill` (`id`, `name`) VALUES
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `phone`, `password`, `first_name`, `middle_name`, `last_name`, `image`, `country`, `county`, `city`, `street`, `postal_code`, `is_admin`, `is_active`, `time_created`) VALUES
-(11, 'byfixusaki', 'cemetugyci@mailinator.com', '+254718578833', '$2y$10$5QzsfhuROuvDUYtQ4Zv3ZuKkVEfaW1txv/553xBCTunpQvc/bpIsK', 'Daniele', NULL, 'Moi', '/uploads/profile.avif', NULL, NULL, NULL, NULL, NULL, 0, 1, '2022-03-22 06:52:13'),
-(12, 'mutuvor', 'kefegi@mailinator.com', '+254762307628', '$2y$10$zL2K68YBS7rwMSGIyQ5R.egPQYy43zMaPokgHtOA01j4ylacVCRPu', 'Sharon', NULL, 'Kemunto', '/uploads/profile-female.avif', NULL, NULL, NULL, NULL, NULL, 0, 1, '2022-03-22 07:53:28'),
+(11, 'byfixusaki', 'client@test.com', '+254718578833', '$2y$10$5QzsfhuROuvDUYtQ4Zv3ZuKkVEfaW1txv/553xBCTunpQvc/bpIsK', 'Daniele', NULL, 'Moi', '/uploads/profile.avif', NULL, NULL, NULL, NULL, NULL, 0, 1, '2022-03-22 06:52:13'),
+(12, 'mutuvor', 'freelancer@test.com', '+254762307628', '$2y$10$zL2K68YBS7rwMSGIyQ5R.egPQYy43zMaPokgHtOA01j4ylacVCRPu', 'Sharon', NULL, 'Kemunto', '/uploads/profile-female.avif', NULL, NULL, NULL, NULL, NULL, 0, 1, '2022-03-22 07:53:28'),
 (13, 'wyjaqe', 'giferuqa@mailinator.com', '+254793395484', '$2y$10$uohjjRXWHVnduYxpyyUuq.e1yNBkS2BZfXI4SvZzRUWCS.DJbXkS6', 'Amity', NULL, 'Bean', '/uploads/ProfileImageTueMar2220229:241628513616264.jpeg', NULL, NULL, NULL, NULL, NULL, 0, 1, '2022-03-22 08:53:44');
 COMMIT;
 
